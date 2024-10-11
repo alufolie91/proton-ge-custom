a7af885802071cee6cf46a6480152daae5a861f4
Alug <alufoliehorst@gmail.com>
2024-10-11 09:53:11 +0200

    just add those to normal patch script lul

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -322,6 +322,16 @@
     echo "WINE: -CUSTOM- Downgrade MESSAGE to TRACE to remove write_watches spam"
     patch -Np1 < ../patches/proton/0001-ntdll-Downgrade-using-kernel-write-watches-from-MESS.patch
 
+    echo "WINE: Add Wine-to-Unix PID mapping"
+    patch -Np1 < ../patches/custom/proton9-unix-pid-maps.patch
+
+    echo "WINE: Add Ping Fix"
+    patch -Np1 < ../patches/custom/ping-fix.patch
+
+    echo "WINE: Add DualSense patches"
+    patch -Np1 < ../patches/custom/dualsense1.patch
+    patch -Np1 < ../patches/custom/dualsense2.patch
+
     popd
 
 ### END PROTON-GE ADDITIONAL CUSTOM PATCHES ###
